efe7f0d628390b55461d6a95e38c1c48b6e0d08b
m-oka-system <m-oka-system@outlook.com>
2025-05-02 09:49:54 +0900

    [update]backendのEntra ID認証を有効化

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate84501"
+    storage_account_name = "stterraformstate84502"
     container_name       = "terraform-state"
     key                  = "dev.terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
