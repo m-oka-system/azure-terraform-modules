ee3c1fc3219d725cd865a2a64bd5a7e2dd31cef0
m-oka-system <m-oka-system@outlook.com>
2025-07-29 21:24:27 +0900

    [add] VNet ピアリングモジュールを追加

---
+++ modules/vnet_peering/variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" {}
+variable "hub_vnet_name" {}
+variable "hub_vnet_id" {}
+variable "spoke_vnet_name" {}
+variable "spoke_vnet_id" {}
---
+++ modules/vnet_peering/vnet_peering.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+  name                         = "${var.hub_vnet_name}-to-${var.spoke_vnet_name}"
+  resource_group_name          = var.resource_group_name
+  virtual_network_name         = var.hub_vnet_name
+  remote_virtual_network_id    = var.spoke_vnet_id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true  # Hub が Spoke から転送されてきたトラフィック（他の Spoke や オンプレミスからのトラフィック）を受け入れるか（Spoke 間通信が必要な場合は true）
+  allow_gateway_transit        = false # Hub が自身の VPN/ExpressRoute Gateway を Spoke に使わせるか（Hub にゲートウェイがある場合のみ有効）
+  use_remote_gateways          = false # Hub が Spoke の VPN/ExpressRoute Gateway を使用するか（通常 Hub では false）
+}
+
+resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+  name                         = "${var.spoke_vnet_name}-to-${var.hub_vnet_name}"
+  resource_group_name          = var.resource_group_name
+  virtual_network_name         = var.spoke_vnet_name
+  remote_virtual_network_id    = var.hub_vnet_id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false # Spoke が他のネットワークから転送されてきたトラフィックを受け入れるか（通常 Spoke では false）
+  allow_gateway_transit        = false # Spoke が自身の VPN/ExpressRoute Gateway を Hub に使わせるか（通常 Spoke では false）
+  use_remote_gateways          = false # Spoke が Hub の VPN/ExpressRoute Gateway を使用するか（Hub にゲートウェイがある場合は true）
+}
