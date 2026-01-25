f8af5f996b4b8e115ad67c728df9f07943bb248a
m-oka-system <m-oka-system@outlook.com>
2026-01-25 18:35:33 +0900

    feat: プライベート DNS ゾーンの VNet リンクにタグを追加

--- modules/private_dns_zone/private_dns_zone.tf
+++ modules/private_dns_zone/private_dns_zone.tf
@@ -15,4 +15,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.this[each.key].name
   virtual_network_id    = var.vnet[var.target_vnet].id
+
+  tags = var.tags
 }
