872906fe0515516da6c75a6606f1c5f54f349cf5
m-oka-system <m-oka-system@outlook.com>
2026-01-25 18:24:08 +0900

    feat: AKS サブネットから PE サブネットへの HTTPS 通信を許可する NSG ルールを追加

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -434,6 +434,18 @@ variable "network_security_rule" {
     },
     {
       target_nsg                 = "pe"
+      name                       = "AllowAksSubnetHTTPSInbound"
+      priority                   = 1200
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "10.10.12.0/24"
+      destination_address_prefix = "*"
+    },
+    {
+      target_nsg                 = "pe"
       name                       = "DenyAllInbound"
       priority                   = 4096
       direction                  = "Inbound"
