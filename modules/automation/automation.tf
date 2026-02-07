8b6b7b182744b5baf2e293eb5d3cb4099aa311f2
m-oka-system <m-oka-system@outlook.com>
2026-02-07 17:50:57 +0900

    feat: Automation Account にリソースグループ名の変数を追加

--- modules/automation/automation.tf
+++ modules/automation/automation.tf
@@ -19,3 +19,12 @@ resource "azurerm_role_assignment" "automation_contributor" {
   role_definition_name = "Contributor"
   principal_id         = azurerm_automation_account.this.identity[0].principal_id
 }
+
+# Automation 変数
+resource "azurerm_automation_variable_string" "resource_group_name" {
+  name                    = "ResourceGroupName"
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.this.name
+  value                   = var.resource_group_name
+  encrypted               = false
+}
