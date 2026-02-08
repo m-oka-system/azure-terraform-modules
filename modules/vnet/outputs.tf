5402b640a408c1cf4a8f94d1877c64a0ac349367
m-oka-system <m-oka-system@outlook.com>
2026-02-08 15:21:51 +0900

    Revert "refactor: vnet モジュールの変数定義を AVM 標準に準拠させる"

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,9 +1,7 @@
 output "vnet" {
-  description = "仮想ネットワークのリソース情報"
-  value       = azurerm_virtual_network.this
+  value = azurerm_virtual_network.this
 }
 
 output "subnet" {
-  description = "サブネットのリソース情報"
-  value       = azurerm_subnet.this
+  value = azurerm_subnet.this
 }
