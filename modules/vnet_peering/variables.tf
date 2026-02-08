919d017810d75845b7be7e1473ec1a7093c2e983
m-oka-system <m-oka-system@outlook.com>
2026-02-08 11:08:17 +0900

    refactor: vnet_peering モジュールの変数に description, type, nullable を追加

--- modules/vnet_peering/variables.tf
+++ modules/vnet_peering/variables.tf
@@ -1,5 +1,29 @@
-variable "resource_group_name" {}
-variable "hub_vnet_name" {}
-variable "hub_vnet_id" {}
-variable "spoke_vnet_name" {}
-variable "spoke_vnet_id" {}
+variable "resource_group_name" {
+  description = "リソースグループ名"
+  type        = string
+  nullable    = false
+}
+
+variable "hub_vnet_name" {
+  description = "Hub VNet の名前"
+  type        = string
+  nullable    = false
+}
+
+variable "hub_vnet_id" {
+  description = "Hub VNet のリソース ID"
+  type        = string
+  nullable    = false
+}
+
+variable "spoke_vnet_name" {
+  description = "Spoke VNet の名前"
+  type        = string
+  nullable    = false
+}
+
+variable "spoke_vnet_id" {
+  description = "Spoke VNet のリソース ID"
+  type        = string
+  nullable    = false
+}
